bd7c54bb73c8e415367db6ad10c7ba5ea46f824b
reniass <reinke.krzysztof@o2.pl>
2022-07-09 13:46:53 +0200

    add spring project

---
+++ src/main/java/com/reinke/linkshortener/LinkShortenerApplication.java
@@ -0,0 +1,14 @@
+package com.reinke.linkshortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkShortenerApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkShortenerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reinke/linkshortener/LinkShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reinke.linkshortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkShortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
