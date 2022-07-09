2dcd0adbf39abeff08f11a2b3cb9b58c62ee39db
reniass <reinke.krzysztof@o2.pl>
2022-07-09 15:20:47 +0200

    created link controllers

---
+++ src/main/java/com/reinke/linkshortener/CreateLinkDto.java
@@ -0,0 +1,28 @@
+package com.reinke.linkshortener;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import java.time.LocalDate;
+
+public class CreateLinkDto {
+    
+    private String email;
+    private String targetUrl;
+    private LocalDate expirationDate;
+    private String id;
+
+    public CreateLinkDto(String email, String targetUrl, LocalDate expirationDate, String id) {
+        this.email = email;
+        this.targetUrl = targetUrl;
+        this.expirationDate = expirationDate;
+        this.id = id;
+    }
+
+    LinkDto toDto() {
+        return new LinkDto(id,
+                expirationDate,
+                0,
+                email,
+                targetUrl);
+    }
+}
---
+++ src/main/java/com/reinke/linkshortener/LinkDto.java
@@ -0,0 +1,70 @@
+package com.reinke.linkshortener;
+
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.time.LocalDate;
+
+public class LinkDto {
+
+    private String id;
+    private LocalDate expirationDate;
+    private int visits;
+    private String email;
+    private String targetUrl;
+
+    public LinkDto(String id, LocalDate expirationDate, int visits, String email, String targetUrl) {
+        this.id = id;
+        this.expirationDate = expirationDate;
+        this.visits = visits;
+        this.email = email;
+        this.targetUrl = targetUrl;
+    }
+
+    public String getShortenedLink() {
+        return ServletUriComponentsBuilder
+                .fromCurrentContextPath()
+                .path("/s/{id}")
+                .buildAndExpand(id)
+                .toUriString();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public LocalDate getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(LocalDate expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    public int getVisits() {
+        return visits;
+    }
+
+    public void setVisits(int visits) {
+        this.visits = visits;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTargetUrl() {
+        return targetUrl;
+    }
+
+    public void setTargetUrl(String targetUrl) {
+        this.targetUrl = targetUrl;
+    }
+}
